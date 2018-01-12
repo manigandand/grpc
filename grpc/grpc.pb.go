59db8c5df50529eca650df2b7fffa34b9e546574
Manigandan Dharmalingam <manigandan.jeff@gmail.com>
2018-01-12 12:27:16 +0530

    proto file genereate.

--- grpc/grpc.pb.go
+++ grpc/grpc.pb.go
@@ -20,7 +20,7 @@ import math "math"
 
 import (
 	context "golang.org/x/net/context"
-	grpc "google.golang.org/grpc"
+	grpc1 "google.golang.org/grpc"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -90,30 +90,30 @@ func init() {
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
-var _ grpc.ClientConn
+var _ grpc1.ClientConn
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion4
+const _ = grpc1.SupportPackageIsVersion4
 
-// Client API for IronMan service
+// Client API for Ironman service
 
 type IronmanClient interface {
-	// RPC Calls for IronMan
-	Ping(ctx context.Context, in *Message, opts ...grpc.CallOption) (*Message, error)
+	// RPC Calls for Ironman
+	Ping(ctx context.Context, in *Message, opts ...grpc1.CallOption) (*Message, error)
 }
 
 type ironmanClient struct {
-	cc *grpc.ClientConn
+	cc *grpc1.ClientConn
 }
 
-func NewIronmanClient(cc *grpc.ClientConn) IronmanClient {
+func NewIronmanClient(cc *grpc1.ClientConn) IronmanClient {
 	return &ironmanClient{cc}
 }
 
-func (c *ironmanClient) Ping(ctx context.Context, in *Message, opts ...grpc.CallOption) (*Message, error) {
+func (c *ironmanClient) Ping(ctx context.Context, in *Message, opts ...grpc1.CallOption) (*Message, error) {
 	out := new(Message)
-	err := grpc.Invoke(ctx, "/grpc.Ironman/Ping", in, out, c.cc, opts...)
+	err := grpc1.Invoke(ctx, "/grpc.Ironman/Ping", in, out, c.cc, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -127,11 +127,11 @@ type IronmanServer interface {
 	Ping(context.Context, *Message) (*Message, error)
 }
 
-func RegisterIronmanServer(s *grpc.Server, srv IronmanServer) {
+func RegisterIronmanServer(s *grpc1.Server, srv IronmanServer) {
 	s.RegisterService(&_Ironman_serviceDesc, srv)
 }
 
-func _Ironman_Ping_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Ironman_Ping_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc1.UnaryServerInterceptor) (interface{}, error) {
 	in := new(Message)
 	if err := dec(in); err != nil {
 		return nil, err
@@ -139,7 +139,7 @@ func _Ironman_Ping_Handler(srv interface{}, ctx context.Context, dec func(interf
 	if interceptor == nil {
 		return srv.(IronmanServer).Ping(ctx, in)
 	}
-	info := &grpc.UnaryServerInfo{
+	info := &grpc1.UnaryServerInfo{
 		Server:     srv,
 		FullMethod: "/grpc.Ironman/Ping",
 	}
@@ -149,36 +149,36 @@ func _Ironman_Ping_Handler(srv interface{}, ctx context.Context, dec func(interf
 	return interceptor(ctx, in, info, handler)
 }
 
-var _Ironman_serviceDesc = grpc.ServiceDesc{
+var _Ironman_serviceDesc = grpc1.ServiceDesc{
 	ServiceName: "grpc.Ironman",
 	HandlerType: (*IronmanServer)(nil),
-	Methods: []grpc.MethodDesc{
+	Methods: []grpc1.MethodDesc{
 		{
 			MethodName: "Ping",
 			Handler:    _Ironman_Ping_Handler,
 		},
 	},
-	Streams:  []grpc.StreamDesc{},
+	Streams:  []grpc1.StreamDesc{},
 	Metadata: "grpc.proto",
 }
 
 // Client API for Batman service
 
 type BatmanClient interface {
-	IronmanCallback(ctx context.Context, in *Session, opts ...grpc.CallOption) (*Void, error)
+	IronManCallback(ctx context.Context, in *Session, opts ...grpc1.CallOption) (*Void, error)
 }
 
 type batmanClient struct {
-	cc *grpc.ClientConn
+	cc *grpc1.ClientConn
 }
 
-func NewBatmanClient(cc *grpc.ClientConn) BatmanClient {
+func NewBatmanClient(cc *grpc1.ClientConn) BatmanClient {
 	return &batmanClient{cc}
 }
 
-func (c *batmanClient) IronmanCallback(ctx context.Context, in *Session, opts ...grpc.CallOption) (*Void, error) {
+func (c *batmanClient) IronManCallback(ctx context.Context, in *Session, opts ...grpc1.CallOption) (*Void, error) {
 	out := new(Void)
-	err := grpc.Invoke(ctx, "/grpc.Batman/IronmanCallback", in, out, c.cc, opts...)
+	err := grpc1.Invoke(ctx, "/grpc.Batman/IronManCallback", in, out, c.cc, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -188,60 +188,60 @@ func (c *batmanClient) IronmanCallback(ctx context.Context, in *Session, opts ..
 // Server API for Batman service
 
 type BatmanServer interface {
-	IronmanCallback(context.Context, *Session) (*Void, error)
+	IronManCallback(context.Context, *Session) (*Void, error)
 }
 
-func RegisterBatmanServer(s *grpc.Server, srv BatmanServer) {
+func RegisterBatmanServer(s *grpc1.Server, srv BatmanServer) {
 	s.RegisterService(&_Batman_serviceDesc, srv)
 }
 
-func _Batman_IronmanCallback_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Batman_IronManCallback_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc1.UnaryServerInterceptor) (interface{}, error) {
 	in := new(Session)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(BatmanServer).IronmanCallback(ctx, in)
+		return srv.(BatmanServer).IronManCallback(ctx, in)
 	}
-	info := &grpc.UnaryServerInfo{
+	info := &grpc1.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/grpc.Batman/IronmanCallback",
+		FullMethod: "/grpc.Batman/IronManCallback",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(BatmanServer).IronmanCallback(ctx, req.(*Session))
+		return srv.(BatmanServer).IronManCallback(ctx, req.(*Session))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-var _Batman_serviceDesc = grpc.ServiceDesc{
+var _Batman_serviceDesc = grpc1.ServiceDesc{
 	ServiceName: "grpc.Batman",
 	HandlerType: (*BatmanServer)(nil),
-	Methods: []grpc.MethodDesc{
+	Methods: []grpc1.MethodDesc{
 		{
-			MethodName: "IronmanCallback",
-			Handler:    _Batman_IronmanCallback_Handler,
+			MethodName: "IronManCallback",
+			Handler:    _Batman_IronManCallback_Handler,
 		},
 	},
-	Streams:  []grpc.StreamDesc{},
+	Streams:  []grpc1.StreamDesc{},
 	Metadata: "grpc.proto",
 }
 
 func init() { proto.RegisterFile("grpc.proto", fileDescriptor0) }
 
 var fileDescriptor0 = []byte{
-	// 211 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x54, 0x8f, 0x41, 0x6b, 0x83, 0x40,
-	0x10, 0x85, 0xab, 0x6c, 0x57, 0x1c, 0xa8, 0x85, 0x39, 0x14, 0x11, 0x5a, 0xca, 0x1e, 0x4a, 0xa1,
-	0x20, 0xad, 0xa5, 0x7f, 0xc0, 0x5e, 0x4a, 0xa1, 0x20, 0x16, 0x72, 0x1f, 0x75, 0x31, 0x4b, 0x56,
-	0x37, 0xb8, 0x9b, 0x90, 0x9f, 0x1f, 0x5c, 0xcd, 0x21, 0xb7, 0xf7, 0x66, 0xf8, 0x78, 0xef, 0x01,
-	0x6c, 0x49, 0xeb, 0x7c, 0x3f, 0x19, 0x67, 0x90, 0xcd, 0x5a, 0x70, 0x60, 0x1b, 0xa3, 0x3a, 0xf1,
-	0x08, 0xd1, 0x9f, 0xb4, 0x96, 0x7a, 0x89, 0x08, 0xcc, 0xc9, 0x93, 0x4b, 0x83, 0xe7, 0xe0, 0x35,
-	0xae, 0xbd, 0x16, 0x1f, 0x10, 0xfd, 0x4b, 0x6b, 0x95, 0x19, 0x31, 0x81, 0x50, 0x75, 0xfe, 0x79,
-	0x5b, 0x87, 0xaa, 0xc3, 0x07, 0xe0, 0xd6, 0x91, 0x3b, 0xd8, 0x34, 0xf4, 0xc0, 0xea, 0x8a, 0x77,
-	0xe0, 0xbf, 0x34, 0x1d, 0x95, 0xc5, 0x17, 0x60, 0x95, 0x1a, 0x7b, 0xbc, 0xcb, 0x7d, 0xfc, 0x9a,
-	0x93, 0x5d, 0x5b, 0x71, 0x53, 0x7c, 0x01, 0x2f, 0xc9, 0x0d, 0x34, 0xe2, 0x1b, 0x24, 0x0b, 0xfb,
-	0x4d, 0x5a, 0x37, 0xd4, 0xee, 0x2e, 0xec, 0x5a, 0x22, 0x83, 0xc5, 0xce, 0xd5, 0xcb, 0x27, 0xb8,
-	0x6f, 0xcd, 0x90, 0x93, 0x9a, 0x5a, 0x67, 0xfc, 0xbd, 0x8c, 0x7f, 0x48, 0xeb, 0x6a, 0x9e, 0x59,
-	0x05, 0x0d, 0xf7, 0x7b, 0x3f, 0xcf, 0x01, 0x00, 0x00, 0xff, 0xff, 0xd6, 0x4b, 0xbc, 0x9e, 0xfd,
-	0x00, 0x00, 0x00,
+	// 214 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x54, 0x8f, 0x41, 0x4b, 0xc4, 0x30,
+	0x14, 0x84, 0x6d, 0x89, 0x29, 0xfb, 0x40, 0x85, 0x20, 0xb2, 0x2c, 0x08, 0x12, 0x41, 0x3c, 0x48,
+	0x61, 0x57, 0xf0, 0x07, 0xd4, 0x83, 0x78, 0x58, 0x28, 0x15, 0xbc, 0xbf, 0x26, 0x21, 0x04, 0xdb,
+	0x97, 0x92, 0x44, 0xf0, 0xe7, 0x4b, 0x62, 0x3d, 0xec, 0x6d, 0x86, 0xe1, 0x63, 0x66, 0x00, 0x6c,
+	0x58, 0x54, 0xbb, 0x04, 0x9f, 0xbc, 0x60, 0x59, 0x4b, 0x0e, 0xec, 0xd3, 0x3b, 0x2d, 0x6f, 0xa1,
+	0x39, 0x9a, 0x18, 0xd1, 0x1a, 0x21, 0x80, 0x25, 0xf3, 0x93, 0xb6, 0xd5, 0x5d, 0xf5, 0xb8, 0x19,
+	0x8a, 0x96, 0x7b, 0x68, 0x3e, 0x4c, 0x8c, 0xce, 0x93, 0xb8, 0x84, 0xda, 0xe9, 0x12, 0x9e, 0x0f,
+	0xb5, 0xd3, 0xe2, 0x06, 0x78, 0x4c, 0x98, 0xbe, 0xe3, 0xb6, 0x2e, 0xc0, 0xea, 0x0e, 0x7b, 0x68,
+	0xde, 0x83, 0xa7, 0x19, 0x49, 0x3c, 0x00, 0xeb, 0x1d, 0x59, 0x71, 0xd1, 0x96, 0xfe, 0xb5, 0x68,
+	0x77, 0x6a, 0xe5, 0xd9, 0xe1, 0x05, 0x78, 0x87, 0x29, 0x13, 0x4f, 0x70, 0x95, 0xe1, 0x23, 0xd2,
+	0x2b, 0x4e, 0xd3, 0x88, 0xea, 0xeb, 0x1f, 0x5e, 0x67, 0xec, 0xe0, 0xcf, 0xe6, 0xf1, 0xdd, 0x3d,
+	0x5c, 0x2b, 0x3f, 0xb7, 0x33, 0x92, 0xb3, 0x48, 0x1a, 0x49, 0x97, 0xb0, 0xdb, 0xbc, 0x85, 0x45,
+	0xf5, 0xf9, 0x6d, 0x5f, 0x8d, 0xbc, 0xdc, 0x7e, 0xfe, 0x0d, 0x00, 0x00, 0xff, 0xff, 0xb3, 0x51,
+	0x3c, 0x63, 0x04, 0x01, 0x00, 0x00,
 }
